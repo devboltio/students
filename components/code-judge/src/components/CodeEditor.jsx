c0d817e0df5b5bcef1a33026bee3aa29dea3920a
morghan102 <morghan102@gmail.com>
2022-01-17 12:10:39 -0800

    codeEditor working, switch to js

--- components/code-judge/src/components/CodeEditor.jsx
+++ components/code-judge/src/components/CodeEditor.jsx
@@ -1,43 +1,47 @@
 import { useState } from 'react';
-// import { useCodeMirror } from '@uiw/react-codemirror';
+import CodeMirror from '@uiw/react-codemirror';
 import 'codemirror/keymap/sublime';
 import '../codeMirrorDracula.css';
-import CodeMirror from '@uiw/react-codemirror';
 // import { javascript } from '@codemirror/lang-javascript';
 import { Container, Row, Button, Col } from 'react-bootstrap';
-import CSS from 'csstype';
+// import CSS from 'csstype';
 
-interface UserAnswer {
-  //not sure where I want this??
-  question: string;
-  answer: string;
-  timeToComplete: number;
-  correct: boolean;
-  languageUsed: string;
-  correctAnswer: string;
-  // if i do decide I want to use this, refer to min 51 for how to save the ans in here
-}
+// interface UserAnswer {
+//   //not sure where I want this??
+//   question: string;
+//   answer: string;
+//   timeToComplete: number;
+//   correct: boolean;
+//   languageUsed: string;
+//   correctAnswer: string;
+//   // if i do decide I want to use this, refer to min 51 for how to save the ans in here
+// }
 
 const CodeEditor = () => {
-  const [code, setCode] = useState<string>('');
+  const [code, setCode] = useState('a = 0');
+  //<string>('');
   const [userAns, setUserAns] = useState(''); //<UserAnswer[]>([]);
-
-  const submit = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setUserAns(e.currentTarget.value);
+  const submit = () => {
+    console.log(code);
+    //e: React.MouseEvent<HTMLButtonElement>
+    // setUserAns(e.currentTarget.value);
     // starts tests, not sure how to embed these
   };
 
   return (
     <Container className="header">
-      <Row>
+      <Row className="codeEditor">
         <CodeMirror
           value={code}
-          onChange={() => setCode(code)}
-          // options={{
-          //   theme: 'dracula',
-          //   keyMap: 'sublime',
-          //   mode: 'jsx',
-          // }}
+          options={{
+            theme: 'dracula',
+            keyMap: 'sublime',
+            mode: 'python',
+          }}
+          // height={height()}
+          onChange={(editor, change) => {
+            setCode(editor);
+          }}
         />
       </Row>
 
